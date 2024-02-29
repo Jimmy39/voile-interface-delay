05062e0cc7342c8489991d8fd1857c6f936bed9b
Jimmy39 <wanghanyu2000@126.com>
2024-02-29 12:15:43 +0800

    rebuild

--- library/voile_interface_delay.h
+++ library/voile_interface_delay.h
@@ -2,24 +2,39 @@
 #define __VOILE_INTERFACE_DELAY_H__
 
 #include "voile_common.h"
+#include "voile_errorno.h"
 
-voile_status_t voile_delay_Init();
+void Vdelay_init();
 
-voile_status_t voile_delay_us(uint16_t);
+void Vdelay_us(uint16_t);
 
-voile_status_t voile_delay_ms(uint16_t);
+void Vdelay_ms(uint16_t);
 
-voile_status_t voile_delay_s(uint16_t);
+void Vdelay_s(uint16_t);
 
-struct splitTime_struct;
-typedef struct splitTime_struct splitTime_t;
+void Vsleep_us(uint16_t);
 
-voile_status_t voile_delay_SplitTime(splitTime_t *);
+void Vsleep_ms(uint16_t);
 
-voile_status_t voile_delay_count_us(splitTime_t *, uint16_t);
+void Vsleep_s(uint16_t);
 
-voile_status_t voile_delay_count_ms(splitTime_t *, uint16_t);
+typedef struct {
+    uint32_t coarse;
+    uint32_t fine;
+} delayBase_t;
 
-voile_status_t voile_delay_count_s(splitTime_t *, uint16_t);
+void Vdelay_getBase(delayBase_t *);
 
-#endif // !__VOILE_INTERFACE_DELAY_H__
+void Vdelay_based_us(delayBase_t *, uint16_t);
+
+void Vdelay_based_ms(delayBase_t *, uint16_t);
+
+void Vdelay_based_s(delayBase_t *, uint16_t);
+
+void Vsleep_based_us(delayBase_t *, uint16_t);
+
+void Vsleep_based_ms(delayBase_t *, uint16_t);
+
+void Vsleep_based_s(delayBase_t *, uint16_t);
+
+#endif  // !__VOILE_INTERFACE_DELAY_H__

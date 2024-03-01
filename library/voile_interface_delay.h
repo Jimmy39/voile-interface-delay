c3eda8b622b4248cc253d9762f607eba62420418
Jimmy39 <wanghanyu2000@126.com>
2024-03-01 18:02:42 +0800

    inline support

--- library/voile_interface_delay.h
+++ library/voile_interface_delay.h
@@ -1,40 +1,50 @@
 #ifndef __VOILE_INTERFACE_DELAY_H__
 #define __VOILE_INTERFACE_DELAY_H__
 
+#define __IN_VOILE_DELAY__
 #include "voile_common.h"
-#include "voile_errorno.h"
+#undef __IN_VOILE_DELAY__
 
 void Vdelay_init();
 
-void Vdelay_us(uint16_t);
+#ifndef __VOILE_DELAY_INLINE
 
-void Vdelay_ms(uint16_t);
+void Vdelay_us(uint16_t time_us);
 
-void Vdelay_s(uint16_t);
+void Vdelay_ms(uint16_t time_ms);
 
-void Vsleep_us(uint16_t);
+void Vdelay_s(uint16_t time_s);
 
-void Vsleep_ms(uint16_t);
+void Vsleep_us(uint16_t time_us);
 
-void Vsleep_s(uint16_t);
+void Vsleep_ms(uint16_t time_ms);
 
+void Vsleep_s(uint16_t time_s);
+
+#ifndef __VOILE_UNSUPPORT_INT64
+typedef uint64_t delayBase_t;
+#else
 typedef struct {
     uint32_t coarse;
     uint32_t fine;
 } delayBase_t;
+#endif  // __VOILE_UNSUPPORT_INT64
 
-void Vdelay_getBase(delayBase_t *);
 
-void Vdelay_based_us(delayBase_t *, uint16_t);
+void Vdelay_getBase(delayBase_t *delayBase_p);
 
-void Vdelay_based_ms(delayBase_t *, uint16_t);
+void Vdelay_based_us(delayBase_t *delayBase_p, uint16_t time_us);
 
-void Vdelay_based_s(delayBase_t *, uint16_t);
+void Vdelay_based_ms(delayBase_t *delayBase_p, uint16_t time_ms);
 
-void Vsleep_based_us(delayBase_t *, uint16_t);
+void Vdelay_based_s(delayBase_t *delayBase_p, uint16_t time_s);
 
-void Vsleep_based_ms(delayBase_t *, uint16_t);
+void Vsleep_based_us(delayBase_t *delayBase_p, uint16_t time_us);
 
-void Vsleep_based_s(delayBase_t *, uint16_t);
+void Vsleep_based_ms(delayBase_t *delayBase_p, uint16_t time_ms);
+
+void Vsleep_based_s(delayBase_t *delayBase_p, uint16_t time_s);
+
+#endif  //!__VOILE_DELAY_INLINE
 
 #endif  // !__VOILE_INTERFACE_DELAY_H__
